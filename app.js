86dc40c4d99d279689a9653c063cae6f4c9fa67b
Abdulaziz Alzaabi <alzaabi98@gmail.com>
2019-05-17 17:13:37 +0400

    register users in

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require("express");
-var exphbs = require("express-handlebars");
-
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 const app = new express();
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const port = 5000;
+//body parser
+//configure session and flash
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+//configure passport
+app.use(flash());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // routes
 app.use("/", require("./routes/index"));
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
-
 const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 //login form
 router.get("/login", (req, res) => {
@@ -11,6 +12,81 @@ router.get("/login", (req, res) => {
 
 router.get("/register", (req, res) => {
   res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  const { email, password, password2 } = req.body;
+  //validation
+
+  let errors = [];
+  if (!email || !password || !password2) {
+    errors.push({
+      msg: "please enter all feilds"
+    });
+  }
+  if (password !== password2) {
+    errors.push({
+      msg: "password does not match"
+    });
+  }
+
+  if (password.length < 3) {
+    errors.push({
+      msg: "password must be more than 3 characteres"
+    });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors });
+  } else {
+    //validationis ok
+    // check if user is exist or not
+    User.findOne({
+      where: {
+        email: email
+      }
+    })
+      .then(user => {
+        if (user) {
+          errors.push({
+            msg: "email was already used"
+          });
+          res.render("register", { errors });
+        } else {
+          //create user
+
+          const newUser = new User({
+            email,
+            password
+          });
+          //hash the password
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) throw err;
+              newUser.password = hash;
+            });
+            newUser
+              .save()
+              .then(user => {
+                console.log("user created");
+                req.flash(
+                  "successMessage",
+                  " your account is created ,please login"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(err => {
+                console.log(err);
+                req.flash("ErrorMessage", "There an error");
+                res.redirect("/users/register");
+              });
+          });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
 
 module.exports = router;
